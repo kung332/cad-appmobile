8dac287270f9da1cbbe97b3a6bd21bb8076efcb4
kung332 <125404332+kung332@users.noreply.github.com>
2023-05-29 17:05:51 -0300

    Add files via upload

---
+++ contas.kt
@@ -0,0 +1,34 @@
+fun main() {
+
+  // divisão
+  var x = 12
+  var y = 3
+
+  println("A divisão é: " + x / y)
+
+  // adição
+  var soma = x + y
+  println("A soma é: " + soma)
+
+  // subtração
+  var sub = x - y
+  println("A subtração é: " + sub)
+
+  // multiplicação
+  var mult = x * y
+  println("A multiplicação é: " + mult)
+
+  // percentual em foco no resto
+  var por = x % y
+  println("O percentual é: " + por)
+
+  // crescente
+  println("O x é: " + x)
+  ++x
+  println("Agora o x é: " + x)
+
+  // decrescente
+  println("O x é: " + x)
+  --x
+  println("Agora o x é: " + x)
+}
